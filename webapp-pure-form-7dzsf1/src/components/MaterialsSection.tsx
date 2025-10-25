37c75908f1f9a851ad3b679b8ccb34624008a26c
Tonivecher1 <99146456+Tonivecher@users.noreply.github.com>
2025-10-25 11:26:26 +0300

    Adjust materials background video sizing

--- webapp-pure-form-7dzsf1/src/components/MaterialsSection.tsx
+++ webapp-pure-form-7dzsf1/src/components/MaterialsSection.tsx
@@ -142,7 +142,9 @@ const MaterialsSection = () => {
       />
       <video
         ref={videoRef}
-        className="pointer-events-none absolute inset-0 h-full w-full object-cover"
+        className="pointer-events-none absolute left-1/2 top-1/2 min-h-full min-w-full -translate-x-1/2 -translate-y-1/2 object-cover"
+        width={1280}
+        height={720}
         autoPlay
         loop
         muted
